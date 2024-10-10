07ac4b967fc7e2ab4af82309a2da7fc2a74d17af
Lê Anh khoa <22162016@student.hcmute.edu.vn>
2024-10-10 21:29:42 +0700

    setup env

---
+++ src/main/java/hisc/com/homework2/HelloApplication.java
@@ -0,0 +1,9 @@
+package hisc.com.homework2;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
